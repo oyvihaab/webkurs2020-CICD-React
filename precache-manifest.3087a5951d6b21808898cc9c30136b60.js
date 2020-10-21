f1807175e7655d80e59329f4654609dc5490b359
Øyvind Håbrekke <ohaabrekke@MacBook-Pro-de-yvind.local>
2020-10-21 17:17:01 +0200

    Updates

---
+++ precache-manifest.3087a5951d6b21808898cc9c30136b60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834b90537c609fa25269d91912c322cb",
+    "url": "/webkurs2020-CICD-React/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "58271da944256cb058c7",
+    "url": "/webkurs2020-CICD-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58271da944256cb058c7",
+    "url": "/webkurs2020-CICD-React/static/js/main.3ff3858f.chunk.js"
+  },
+  {
+    "revision": "580e99ae8bc13b712167",
+    "url": "/webkurs2020-CICD-React/static/js/runtime-main.89e14c0b.js"
+  }
+]);
